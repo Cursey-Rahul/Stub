4f65b2475e6df0e40f1e2fd24761e3f767e068a2
Cursey-Rahul <cursey.pvt.ltd@gmail.com>
2026-04-14 13:58:20 +0530

    .

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 }
 
 const app = initializeApp(firebaseConfig)
